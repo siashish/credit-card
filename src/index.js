1b16f00ac047c5a014a5acce172cffef8c52c591
Ashish Singh <ashishsingh21798@gmail.com>
2021-09-05 15:45:47 +0530

    credit card UI

---
+++ src/index.js
@@ -0,0 +1,88 @@
+import React from 'react'
+import { render } from 'react-dom'
+import Styles from './Styles'
+import { Form, Field } from 'react-final-form'
+import Card from './Card'
+import {
+  formatCreditCardNumber,
+  formatCVC,
+  formatExpirationDate
+} from './cardUtils'
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
+
+const onSubmit = async values => {
+  await sleep(300)
+  window.alert(JSON.stringify(values, 0, 2))
+}
+
+const App = () => (
+  <Styles>
+    <Form
+      onSubmit={onSubmit}
+      render={({
+        handleSubmit,
+        form,
+        submitting,
+        pristine,
+        values,
+        active
+      }) => {
+        return (
+          <form onSubmit={handleSubmit}>
+            <Card
+              number={values.number || ''}
+              name={values.name || ''}
+              expiry={values.expiry || ''}
+              cvc={values.cvc || ''}
+              focused={active}
+            />
+            <div>
+              <Field
+                name="number"
+                component="input"
+                type="text"
+                pattern="[\d| ]{16,22}"
+                placeholder="Card Number"
+                format={formatCreditCardNumber}
+              />
+            </div>
+            <div>
+              <Field
+                name="name"
+                component="input"
+                type="text"
+                placeholder="Name"
+              />
+            </div>
+            <div>
+              <Field
+                name="expiry"
+                component="input"
+                type="text"
+                pattern="\d\d/\d\d"
+                placeholder="Valid Thru"
+                format={formatExpirationDate}
+              />
+              <Field
+                name="cvc"
+                component="input"
+                type="text"
+                pattern="\d{3,4}"
+                placeholder="CVC"
+                format={formatCVC}
+              />
+            </div>
+            <div className="buttons">
+              <button type="submit" disabled={submitting}>
+                Submit
+              </button>
+            </div>
+          </form>
+        )
+      }}
+    />
+  </Styles>
+)
+
+render(<App />, document.getElementById('root'))
